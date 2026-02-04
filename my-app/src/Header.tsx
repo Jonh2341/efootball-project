d68488e2e65ec087a695fcd0dc03d76699f43009
Adam <rokakaak@email.com>
2026-02-04 16:39:57 +0200

    init New missing img bg for news <a> hover +1 season

--- my-app/src/Header.tsx
+++ my-app/src/Header.tsx
@@ -1,16 +1,16 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 function Header() {
     return (
         <>
             <header className="header-box relative w-full h-[120px] bg-[#D9D9D9] flex items-center justify-between p-[20px] z-[3]">
-                <h1 className="title">transfermarket</h1>
+                <h1 className="title text-4xl">transfermarket</h1>
                 <nav className="navigation w-[30%] flex justify-between">
-                    <a href="" className="link-a text-black border-b">clubs page</a>
-                    <a href="" className="link-a text-black border-b">players page</a>
-                    <a href="" className="link-a text-black border-b">transfers page</a>
+                    <a href="" className="link-a text-black transform transition hover:scale-110 border-b">clubs page</a>
+                    <a href="" className="link-a text-black transform transition hover:scale-110 border-b">players page</a>
+                    <a href="" className="link-a text-black transform transition hover:scale-110 border-b">transfers page</a>
                 </nav>
                 <span className="league text-[#7F7F7F] text-[36px]">Bundesliga</span>
             </header>
--- my-app/src/Home.tsx
+++ my-app/src/Home.tsx
@@ -6,11 +6,13 @@ import seasonStats from "./storage/seasonStats.json";
 import newsDataRaw from "./storage/news.json";
 
 // 🔑 Типи для ключів сезону та структури новин
-type SeasonKey = "seasonTwentySix" | "seasonTwentySeven";
+type SeasonKey = "seasonTwentySix" | "seasonTwentySeven" | "seasonTwentyEight";
 
+// інтерфейс
 interface NewsBlock {
   seasonTwentySix: string;
   seasonTwentySeven: string;
+  seasonTwentyEight: string;
 }
 
 interface NewsData {
@@ -20,34 +22,41 @@ interface NewsData {
   BottomSideNews: NewsBlock;
 }
 
+// підставляємо наші новини під інтерфейс
 const newsData = newsDataRaw as NewsData;
 
 function Home() {
   const [isClicked, setIsClicked] = useState(false);
-  const [seasonAmount, setSeasonAmount] = useState(26);
+  const [seasonStartAmount, setSeasonStartAmount] = useState(26);
 
   // утиліта: число -> ключ сезону
   const numberToSeasonKey = (n: number): SeasonKey => {
     const map: Record<number, SeasonKey> = {
       26: "seasonTwentySix",
       27: "seasonTwentySeven",
+      28: "seasonTwentyEight",
+      
     };
     return map[n];
   };
 
-  const seasonKey = numberToSeasonKey(seasonAmount);
+  // сезонні змінні
+  const seasonKey = numberToSeasonKey(seasonStartAmount);
 
   const seasonAmountCounter = (arr: object) => Object.keys(arr);
   const seasonsLength = seasonAmountCounter(seasonStats).length - 1;
 
+  // додавання кількості сезонів
   const incrementByOne = () => {
-    if (seasonAmount !== 26 + seasonsLength) {
-      setSeasonAmount(prev => prev + 1);
+    if (seasonStartAmount !== 26 + seasonsLength) {
+      setSeasonStartAmount(prev => prev + 1);
     }
   };
+
+  // віднімання кількості сезонів
   const decrementByOne = () => {
-    if (seasonAmount > 26) {
-      setSeasonAmount(prev => prev - 1);
+    if (seasonStartAmount > 26) {
+      setSeasonStartAmount(prev => prev - 1);
     } else {
       alert('Currently this is minimal season.');
     }
@@ -77,6 +86,7 @@ function Home() {
     },
   };
 
+  // функція показування новин
   const showImage = (e: React.MouseEvent<HTMLDivElement>) => {
     const $img = $(e.currentTarget);
 
@@ -100,46 +110,47 @@ function Home() {
     }
   };
 
+  // створюваш новин
   const createNews = (
     hugeNewUrl: string,
     topLeftNewUrl: string,
     littleNewUrl: string,
     bottomSideNewUrl: string
   ) => {
-    return {
-      hugeNew: (
-        <div
-          id="huge-new-id"
-          className="huge-new bg-cover bg-no-repeat w-full h-full"
-          style={{ backgroundImage: `url(${hugeNewUrl})` }}
-          onClick={showImage}
-        ></div>
-      ),
-      topLeftNew: (
-        <div
-          id="top-left-new"
-          className="top-left-new bg-no-repeat w-full h-full bg-cover"
-          style={{ backgroundImage: `url(${topLeftNewUrl})` }}
-          onClick={showImage}
-        ></div>
-      ),
-      littleNew: (
-        <div
-          id="little-new"
-          className="little-new w-full h-full bg-cover bg-no-repeat"
-          style={{ backgroundImage: `url(${littleNewUrl})` }}
-          onClick={showImage}
-        ></div>
-      ),
-      bottomSideNew: (
-        <div
-          id="bottom-side-new"
-          className="bottom-side-new w-full h-full bg-cover bg-no-repeat"
-          style={{ backgroundImage: `url(${bottomSideNewUrl})` }}
-          onClick={showImage}
-        ></div>
-      ),
-    };
+      return {
+        hugeNew: (
+          <div
+            id="huge-new-id"
+            className="huge-new bg-cover bg-no-repeat w-full h-full"
+            style={{ backgroundImage: `url(${hugeNewUrl})` }}
+            onClick={showImage}
+          ></div>
+        ),
+        topLeftNew: (
+          <div
+            id="top-left-new"
+            className="top-left-new bg-no-repeat w-full h-full bg-cover"
+            style={{ backgroundImage: `url(${topLeftNewUrl})` }}
+            onClick={showImage}
+          ></div>
+        ),
+        littleNew: (
+          <div
+            id="little-new"
+            className="little-new w-full h-full bg-cover bg-no-repeat"
+            style={{ backgroundImage: `url(${littleNewUrl})` }}
+            onClick={showImage}
+          ></div>
+        ),
+        bottomSideNew: (
+          <div
+            id="bottom-side-new"
+            className="bottom-side-new w-full h-full bg-cover bg-no-repeat"
+            style={{ backgroundImage: `url(${bottomSideNewUrl})` }}
+            onClick={showImage}
+          ></div>
+        ),
+      };
   };
 
   // динамічні новини
@@ -150,14 +161,15 @@ function Home() {
     newsData.BottomSideNews[seasonKey]
   );
 
+  // виграш кубку
   const cupWinner = Object.values(seasonStats[seasonKey]).find(team => team.cupStatus == 'winner');
 
   return (
     <>
-      <main className="home-container flex justify-between w-full h-620px">
-        <section className="latest-news w-[80%] flex flex-col pt-[26px] pl-[38px] mb-[25px]">
-          <span className="about text-[20px] mb-[17px]">Latest News:</span>
-          <div className="news flex h-[525px]">
+      <main className="home-container flex justify-between w-full h-[620px]">
+        <section className="latest-news w-[80%] flex flex-col pt-[26px] pl-[36px] mb-[25px]">
+          <span className="about w-[130px] bg-white p-[5px] text-[20px] mb-[17px]">Latest News:</span>
+          <div className="news bg-white p-[10px] flex h-[525px]">
             <div className="huge-box w-[32%] mr-[10px]">
               {hugeNew}
             </div>
@@ -168,16 +180,16 @@ function Home() {
                 </div>
                 <div className="top-right-new w-[49%] flex flex-col text-center">
                   <div className="roller-box flex mt-[26px] justify-center">
-                    <button className="previousSeason mr-[15px!important]" onClick={decrementByOne}>&lt;</button>
-                    <div className="roller">season {seasonAmount}</div>
-                    <button className="nextSeason ml-[15px!important]" onClick={incrementByOne}>&gt;</button>
+                    <button className="previousSeason !mr-[15px]" onClick={decrementByOne}>&lt;</button>
+                    <div className="roller">season {seasonStartAmount}</div>
+                    <button className="nextSeason !ml-[15px]" onClick={incrementByOne}>&gt;</button>
                   </div>
                   <div className="little-new-box h-[251px] mt-[24px]">
                     {littleNew}
                   </div>
                 </div>
               </div>
-              <div className="bottom-side-new-box w-full h-[188px]">
+              <div className="bottom-side-new-box w-full h-[188px] mt-[5px]">
                 {bottomSideNew}
               </div>
             </div>
@@ -188,20 +200,20 @@ function Home() {
           <div className="league-table">
             {/* Headers */}
             <div className="header">#</div>
+            <div className="header">G</div>
             <div className="header">W</div>
             <div className="header">L</div>
-            <div className="header">S</div>
-            <div className="header">M</div>
+            <div className="header">D</div>
             <div className="header">P</div>
 
             {/* Rows */}
             {Object.entries(seasonStats[seasonKey] || {}).map(([team, stats]) => (
               <React.Fragment key={team}>
                 <div className="cell"><img src={`../src/storage/${stats.icon}`} /></div>
+                <div className="cell">{stats.wins + stats.loses}</div>
                 <div className="cell">{stats.wins}</div>
                 <div className="cell">{stats.loses}</div>
-                <div className="cell">{stats.scored}</div>
-                <div className="cell">{stats.missed}</div>
+                <div className="cell">{stats.draws}</div>
                 <div className="cell">{stats.wins * 3}</div>
               </React.Fragment>
             ))}
